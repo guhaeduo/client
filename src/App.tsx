391b7f90dc89409cf26b3a1205315eda7345ad34
myeongin <mungin0926@naver.com>
2024-02-16 20:01:50 +0900

    [feat] SASS, classnames 라이브러리 다운로드

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import styles from './hello.module.scss';
+import classNames from 'classnames/bind';
+
+const cn = classNames.bind(styles);
+
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1 className={cn('title')}>hello</h1>
     </div>
   );
 }
