cb747835efdfad62869634d3e59d32442e45bc05
myeongin <mungin0926@naver.com>
2024-02-20 16:35:52 +0900

    [feat] 챔피언 아이콘, 툴팁 디자인 수정

--- src/pages/summonerSearch/component/summonerGameSummary/GameSummaryTag.tsx
+++ src/pages/summonerSearch/component/summonerGameSummary/GameSummaryTag.tsx
@@ -1,6 +1,11 @@
 import { SummaryChampionStats } from 'types/summoner';
 import getChampionData from 'utils/getChampionData';
 import ChampionIcon from 'components/championIcon/ChampionIcon';
+import styles from './summonerGameSummaryContainer.module.scss';
+import classNames from 'classnames/bind';
+
+const cn = classNames.bind(styles);
+
 type Props = {
   champion: SummaryChampionStats;
 };
@@ -9,11 +14,16 @@ export default function GameSummaryTag({ champion }: Props) {
   const championDetail = getChampionData(champion.championName);
   console.log(championDetail);
   return (
-    <div>
+    <div className={cn('championTag')}>
       <ChampionIcon
+        className={cn('championIcon')}
         championId={championDetail.id}
         championName={championDetail.name}
       />
+      <div className={cn('championTagData')}>
+        <span>{championDetail.name}</span>
+        <span>{champion.winningRate}</span>
+      </div>
     </div>
   );
 }
