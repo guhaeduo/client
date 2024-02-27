190144410961e74d47f66527ff8d4c8f0fd71dc2
myeongin <mungin0926@naver.com>
2024-02-27 21:38:10 +0900

    [fix] 이미지 경로 수정

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -4,13 +4,13 @@ const URL = {
   profileIcon: (iconId: number) =>
     `https://ddragon.leagueoflegends.com/cdn/${VERSION}/img/profileicon/${iconId}.png`,
   tierIcon: (tierName: string) =>
-    `${process.env.REACT_APP_PUBLIC_URL}/images/tier/${tierName}.png`,
+    `${process.env.PUBLIC_URL}/images/tier/${tierName}.png`,
   championIcon: (championId: string) =>
     `https://ddragon.leagueoflegends.com/cdn/${VERSION}/img/champion/${championId}.png`,
   itemIcon: (itemNumber: string) =>
     `https://ddragon.leagueoflegends.com/cdn/${VERSION}/img/item/${itemNumber}.png`,
   laneIcon: (lane: string) =>
-    `${process.env.REACT_APP_PUBLIC_URL}/images/lane/${lane}.png`,
+    `${process.env.PUBLIC_URL}/images/lane/${lane}.png`,
   spellIcon: (spellId: string) =>
     `https://ddragon.leagueoflegends.com/cdn/${VERSION}/img/spell/${spellId}.png`,
   perksIcon: (url: string) =>
