9a197b811ff97952f2eb305a0a3cff822846aa61
myeongin <mungin0926@naver.com>
2024-03-15 00:26:13 +0900

    [design] homePage 간격 조정

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -9,11 +9,11 @@ const cn = classNames.bind(styles);
 export default function HomePage() {
   const { navHome } = useCustomNavigation();
   return (
-    <div className={`${cn('homeMain')} container`}>
-      <div className={cn('homeSideImage')}>
-        <img src={process.env.PUBLIC_URL + `/images/akali.png`} alt="" />
-      </div>
+    <div className={`${cn('homeMain', 'centerContainer')}`}>
       <div className={cn('searchArea')}>
+        <div className={cn('homeSideImage')}>
+          <img src={process.env.PUBLIC_URL + `/images/akali.png`} alt="" />
+        </div>
         <h1 onClick={navHome}>구해듀오</h1>
         <SearchBar type="main" />
       </div>
