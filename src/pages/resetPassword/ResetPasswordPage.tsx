d3e0235497daa39dd0c378bfbf652cbce7709bab
myeongin <mungin0926@naver.com>
2024-03-15 13:55:03 +0900

    [design] footer , main 태그 영역 조정

--- src/pages/resetPassword/ResetPasswordPage.tsx
+++ src/pages/resetPassword/ResetPasswordPage.tsx
@@ -3,5 +3,5 @@ import styles from './resetPasswordPage.module.scss';
 import classNames from 'classnames/bind';
 const cn = classNames.bind(styles);
 export default function ResetPasswordPage() {
-  return <div>비밀번호 재설정 페이지</div>;
+  return <div className="centerContainer">비밀번호 재설정 페이지</div>;
 }
