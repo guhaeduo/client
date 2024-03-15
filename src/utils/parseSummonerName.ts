ae4a479b76a851759d2a56d4bd7478ba79347125
myeongin <mungin0926@naver.com>
2024-03-15 13:45:07 +0900

    [feat] 소환사 검색 태그 미 입력시 임의이 태그 생성

--- src/utils/parseSummonerName.ts
+++ src/utils/parseSummonerName.ts
@@ -12,16 +12,14 @@ export default function parseSummonerName(input: string) {
   // 아무것도 입력하지 않았을 때 에러처리
   if (input.trim() === '') throw new Error(ERROR_MESSAGE.INVALID_NAME);
 
-  // 구분자가 없다면 잘못된 태그 에러 발생
-  if (index === -1) throw new Error(ERROR_MESSAGE.INVALID_TAG);
+  // 태그가 없다면 임의의 태그를 리턴
+  if (index === -1) return { name: input, tag: 'KR1' };
 
   // 이름이 없다면 잘못된 이름 에러 발생
   const name = input.substring(0, index);
   if (!name) throw new Error(ERROR_MESSAGE.INVALID_NAME);
 
-  // 태그가 없다면 잘못된 태그 에러 발생
   const tag = input.substring(index + 1);
-  if (!tag) throw new Error(ERROR_MESSAGE.INVALID_TAG);
 
   // 모두 통과했다면, 이름과 태그 반환
   return { name, tag };
