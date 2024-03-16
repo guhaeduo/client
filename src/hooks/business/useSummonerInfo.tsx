3eed6b50ddbaf1026c7b515cc72d32d0cc1ab5d3
myeongin <mungin0926@naver.com>
2024-03-17 01:51:09 +0900

    [docs] useSummonerInfo 주석 작성

--- src/hooks/business/useSummonerInfo.tsx
+++ src/hooks/business/useSummonerInfo.tsx
@@ -1,13 +1,21 @@
 import { useQuery } from '@tanstack/react-query';
-
 import getSummonerInfo from 'service/getSummonerInfo';
 import { SummonerInfo } from 'types/summoner';
 import { SUMMONER_DATA_STALE_TIME } from 'constants/api';
+
 type Props = {
   country: string;
   name: string;
   tag: string;
 };
+
+/**
+ * 소환사의 정보를 받아오는 훅입니다.
+ * @param {string} country - 소환사 검색 국가입니다.
+ * @param {string} name - 소환사 이름입니다.
+ * @param {string} tag - 소환사 태그입니다.
+ */
+
 export default function useSummonerInfo({ country, name, tag }: Props) {
   const {
     data: summonerInfo,
