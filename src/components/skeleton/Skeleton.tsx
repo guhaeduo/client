9cb6695039b1080229d9140a2a7e705e17981ed6
myeongin <mungin0926@naver.com>
2024-03-17 01:28:49 +0900

    [style] Skeleton 주석 작성

--- src/components/skeleton/Skeleton.tsx
+++ src/components/skeleton/Skeleton.tsx
@@ -8,6 +8,11 @@ type Props = {
   className: string;
 };
 
+/**
+ * 스켈레톤 입니다.
+ * @param {string} className - 스켈레톤 클래스입니다.
+ */
+
 export default function Skeleton({ className }: Props) {
   return <div className={cn(className, 'skeleton')}></div>;
 }
