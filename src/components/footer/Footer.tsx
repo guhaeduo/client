c16a138ee75b11b59d60af7b46f9c26382e89182
myeongin <mungin0926@naver.com>
2024-03-18 19:22:11 +0900

    [design] Footer 디자인 수정

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import styles from './footer.module.scss';
 import classNames from 'classnames/bind';
-
+import { Link } from 'react-router-dom';
+import PATH from 'constants/path';
 const cn = classNames.bind(styles);
 
 /**
@@ -11,11 +11,18 @@ const cn = classNames.bind(styles);
 export default function Footer() {
   return (
     <footer className={cn('footer')}>
+      <div>
+        <Link to={PATH.PRIVACY_POLICY} target="_blank">
+          개인정보처리방침
+        </Link>
+        <Link to={PATH.TEAM_OF_USE}>이용약관</Link>
+      </div>
       <p>
         GuhaeDuo isn’t endorsed by Riot Games and doesn’t reflect the views or
         opinions of Riot Games or anyone officially involved in producing or
         managing League of Legends.
       </p>
+      <img src={process.env.PUBLIC_URL + '/images/logo.png'} alt="" />
     </footer>
   );
 }
