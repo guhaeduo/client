16dd8275238d99b1408cbaebadb467400a22456d
myeongin <mungin0926@naver.com>
2024-03-19 15:46:07 +0900

    [design] SummonerSearchInfoContainer 반응형 디자인

--- src/pages/summonerSearch/summonerInfoContainer/SummonerInfoContainer.tsx
+++ src/pages/summonerSearch/summonerInfoContainer/SummonerInfoContainer.tsx
@@ -4,6 +4,7 @@ import classNames from 'classnames/bind';
 import { SummonerInfo, SummonerRankInfo } from 'types/summoner';
 import URL from 'constants/url';
 import { calculateWinRate } from 'utils/calculate';
+import { RankData } from 'types/summoner';
 const cn = classNames.bind(styles);
 
 type Props = {
@@ -19,57 +20,76 @@ export default function SummonerInfoContainer({
 }: Props) {
   const [rankType, setRankType] = useState<RankType>('soloRank');
   const { gameName, tagLine, profileIconId } = summonerInfo;
-  const rankData = summonerRankInfo[rankType];
-  const winningRate = calculateWinRate(rankData.win, rankData.lose);
 
-  return (
-    <div className={cn('summonerInfo')}>
-      <div className={cn('summonerIcon')}>
-        <img src={URL.profileIcon(profileIconId)} alt="summonerProfileIcon" />
+  const renderRankTierContainer = (rankData: RankData) => (
+    <div className={cn('rankDataContainer')}>
+      <div className={cn('rankTierIcon')}>
+        <img src={URL.tierIcon(rankData.tier)} alt="summonerTier" />
       </div>
-      <div className={cn('summonerNameTag')}>
-        <span className={cn('summonerName')}>{gameName}</span>
-        <span className={cn('summonerTag')}>#{tagLine}</span>
-      </div>
-      <div className={cn('rankTierContainer')}>
-        <div className={cn('rankTypeSelectWrapper')}>
-          <span
-            onClick={() => setRankType('soloRank')}
-            className={cn('rankTypeSelector', {
-              active: rankType === 'soloRank',
-            })}
-          >
-            솔로랭크
+      <div className={cn('rankWrapper')}>
+        <p className={cn('rankTier')}>
+          <span>{rankData.tier}</span>
+          {rankData.level !== 0 && <span>{rankData.level}</span>}
+        </p>
+        <span className={cn('rankPoint')}>{rankData.point} LP</span>
+        <p className={cn('winningWrapper')}>
+          <span>승률 {calculateWinRate(rankData.win, rankData.lose)}</span>
+          <span>
+            ({rankData.win}승 {rankData.lose}패)
           </span>
-          <div className={cn('divider')}></div>
-          <span
-            onClick={() => setRankType('freeRank')}
-            className={cn('rankTypeSelector', {
-              active: rankType === 'freeRank',
-            })}
-          >
-            자유랭크
-          </span>
-        </div>
-        <div className={cn('rankDataContainer')}>
-          <div className={cn('rankTierIcon')}>
-            <img src={URL.tierIcon(rankData.tier)} alt="summonerTier" />
-          </div>
-          <div className={cn('rankWrapper')}>
-            <p className={cn('rankTier')}>
-              <span>{rankData.tier}</span>
-              {rankData.level !== 0 && <span>{rankData.level}</span>}
-            </p>
-            <span className={cn('rankPoint')}>{rankData.point} LP</span>
-            <p className={cn('winningWrapper')}>
-              <span>승률 {winningRate}</span>
-              <span>
-                ({rankData.win}승 {rankData.lose}패)
-              </span>
-            </p>
-          </div>
-        </div>
+        </p>
       </div>
     </div>
+  );
+
+  return (
+    <>
+      <div className={cn('summonerInfo')}>
+        <div>
+          <div className={cn('summonerIcon')}>
+            <img
+              src={URL.profileIcon(profileIconId)}
+              alt="summonerProfileIcon"
+            />
+          </div>
+          <div className={cn('summonerNameTag')}>
+            <span className={cn('summonerName')}>{gameName}</span>
+            <span className={cn('summonerTag')}>#{tagLine}</span>
+          </div>
+        </div>
+        <div className={cn('rankTierContainer')}>
+          <div className={cn('rankTypeSelectWrapper')}>
+            <span
+              onClick={() => setRankType('soloRank')}
+              className={cn('rankTypeSelector', {
+                active: rankType === 'soloRank',
+              })}
+            >
+              솔로랭크
+            </span>
+            <div className={cn('divider')}></div>
+            <span
+              onClick={() => setRankType('freeRank')}
+              className={cn('rankTypeSelector', {
+                active: rankType === 'freeRank',
+              })}
+            >
+              자유랭크
+            </span>
+          </div>
+          {renderRankTierContainer(summonerRankInfo[rankType])}
+        </div>
+        <div className={cn('rankTierContainerSpread')}>
+          <div>
+            <div className={cn('title')}>솔로랭크</div>
+            {renderRankTierContainer(summonerRankInfo.soloRank)}
+          </div>
+          <div>
+            <div className={cn('title')}>자유랭크</div>
+            {renderRankTierContainer(summonerRankInfo.freeRank)}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
