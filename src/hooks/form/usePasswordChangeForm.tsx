d65a6f20d07909e79f5ccd1f26f76cb056b9877c
myeongin <mungin0926@naver.com>
2024-03-26 23:03:37 +0900

    [docs] usePasswordChangeForm 주석 작성

--- src/hooks/form/usePasswordChangeForm.tsx
+++ src/hooks/form/usePasswordChangeForm.tsx
@@ -5,6 +5,7 @@ import hasAlphaNumeric from 'utils/hasAlphaNumeric';
 import Toast from 'utils/toast';
 import MESSAGE from 'constants/message';
 import { UNKNOWN_NET_ERROR_MESSAGE } from 'constants/api';
+
 interface FormValue {
   currentPassword: string;
   newPassword: string;
@@ -15,38 +16,57 @@ type Props = {
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
+/**
+ * 비밀번호 변경 모달입니다.
+ * @param {React.Dispatch<React.SetStateAction<boolean>>} setIsModalOpen - 비밀번호 변경 모달을 열고 닫는 핸들러
+ */
+
 export default function usePasswordChangeForm({ setIsModalOpen }: Props) {
   const { register, handleSubmit, watch } = useForm<FormValue>({
     mode: 'onChange',
   });
+  // 현재 Form의 데이터를 실시간으로 감지합니다.
   const currentPassword = watch('currentPassword');
   const newPassword = watch('newPassword');
   const newPasswordCheck = watch('newPasswordCheck');
 
-  const isLengthValid = newPassword?.length > 7;
-  const isHasAlphaNumericValid = hasAlphaNumeric(newPassword);
-  const isMatch = newPassword && newPassword === newPasswordCheck;
-  const isDiffrentValid =
-    newPassword?.length > 7 &&
-    currentPassword?.length > 7 &&
-    currentPassword !== newPassword;
+  // 비밀번호 길이 유효성 검사
+  const isLengthValid = currentPassword?.length > 7 && newPassword?.length > 7;
 
+  // 비밀번호 숫자를 포함하고 있는지 유효성 검사
+  const isHasAlphaNumericValid = hasAlphaNumeric(newPassword);
+
+  // 새로운 비밀번호와 비밀번호 재입력이 일치하는지 유효성 검사
+  const isMatch = newPassword && newPassword === newPasswordCheck;
+
+  // 현재 비밀번호와 새로운 비밀번호가 다른지 유효성 검사
+  const isDiffrentValid = currentPassword !== newPassword;
+
+  // 모든 유효성 검사가 통과했는지 관리하는 변수
   const isValid =
     isLengthValid && isHasAlphaNumericValid && isMatch && isDiffrentValid;
+
+  // 비밀번호 변경 요청을 보내는 함수
   const submitHandler = handleSubmit(async (data) => {
+    // 만약 하나라도 유효성 검사 통과하지 못했을 시 함수 종료
     if (
       !(isLengthValid && isHasAlphaNumericValid && isMatch && isDiffrentValid)
     )
       return;
+
     const { currentPassword, newPassword } = data;
     try {
+      // 비밀번호 변경 요청
       await instance.patch('/api/site/update-password', {
         beforePassword: currentPassword,
         afterPassword: newPassword,
       });
+
+      // 비밀번호 변경 성공시 모달을 닫고 변경 완료 메세지 띄워주기
       setIsModalOpen(false);
       Toast.success(MESSAGE.PASSWORD_CHANGE_SUCCESS);
     } catch (err) {
+      // 비밀번호 변경 실패시 에러 토스트 처리
       if (isCustomAxiosError(err) && err.response) {
         Toast.error(err.response.data.message);
       }
