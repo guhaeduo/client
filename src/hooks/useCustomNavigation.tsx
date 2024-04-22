1760d24f3d4ba0740d94783821977c36171de5c3
myeongin <mungin0926@naver.com>
2024-04-22 21:04:02 +0900

    [docs] useCustomNavigation 주석 작성

--- src/hooks/useCustomNavigation.tsx
+++ src/hooks/useCustomNavigation.tsx
@@ -1,8 +1,16 @@
 import { useNavigate } from 'react-router-dom';
 import PATH from 'constants/path';
 /**
- * 어플리케이션에서 사용될 라우팅 함수를 모아둔 훅입니다.
+ * 어플리케이션에서 사용될 네비게이션 함수를 모아둔 훅입니다.
+ * @return {() => void} navHome - 홈 페이지로 이동하는 네비게이션 함수입니다.
+ * @return {() => void} navLogin - 로그인 페이지로 이동하는 네비게이션 함수입니다.
+ * @return {() => void} navSignup - 회원가입 페이지로 이동하는 네비게이션 함수입니다.
+ * @return {(email?: string) => void} navResetPassword - 비밀번호 초기화 이메일 전송 페이지로 이동하는 네비게이션 함수입니다.
+ * @return {() => void} navProfile - 프로필 페이지로 이동하는 네비게이션 함수입니다.
+ * @return {({ country, name, tag, }: { country: string; name: string; tag: string; }) => void} navSummonerSearch - 소환사 검색 페이지로 이동하는 네비게이션 함수입니다.
+ * @return {() => void} navFindDuo - 듀오 찾기 페이지로 이동하는 네비게이션 함수입니다.
  */
+
 export default function useCustomNavigation() {
   const navigate = useNavigate();
 
